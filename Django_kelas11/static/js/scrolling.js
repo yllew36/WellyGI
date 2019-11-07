4cb0e4b103aa8c1f0312e167c22c095596197a79
yllew36 <yllew36@gmail.com>
2019-11-07 11:08:14 +0700

    Rename

--- Django_kelas11/static/js/scrolling.js
+++
@@ -1,9 +0,0 @@
-$(window).scroll(function() {
-    var scroll = $(window).scrollTop();
-    document.getElementById("myBody").style.marginTop = (-100 - 0.5 * scroll) + "px";
-
-    if (scroll >= 300) {
-        $("#mynav").addClass("bg-dark");
-    } else
-        $("#mynav").removeClass("bg-dark");
-});
